7a2b86af8a10b4788a8b09bd40c9af2282bf97fc
Miia Nyy <66413480+MiiaNyy@users.noreply.github.com>
2022-02-23 16:25:56 +0200

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+
+import profileImg from "./assets/profile.jpg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <Navigation/>
+            <main className="c-container">
+                <div className="hero">
+                    <section className="hero__section">
+                        <h1 className="hero__title">Hi, I'm Miia Nyyssönen</h1>
+                        <p className="hero__info">Software Developer</p>
+                        <p className="hero__info">and traveler || chef || dog servant || optimist || cleaning freak || huge Friends fan</p>
+                    </section>
+                </div>
+
+                <section className="about">
+                    <h2 className="section-header text-start">About</h2>
+                    <div className="d-flex justify-content-between">
+                        <div>
+                            <p>Who I am?</p>
+                            <small>Miia Nyyssönen - est 1992</small>
+                            <div className="about__img-container">
+                                <img width={ 200 } src={ profileImg } alt="Miia's face"/>
+                            </div>
+                        </div>
+                        <div className="flex-col-2">
+                            <strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis nam omnis! At
+                                commodi corporis deleniti dicta eaque incidunt iste modi nobis odit quasi quia quidem
+                                ratione similique tempore, voluptatibus.
+                            </strong>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis nam omnis! At
+                                commodi corporis deleniti dicta eaque incidunt iste modi nobis odit quasi quia quidem
+                                ratione similique tempore, voluptatibus.
+                            </p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam debitis nam omnis! At
+                                commodi corporis deleniti dicta eaque incidunt iste modi nobis odit quasi quia quidem
+                                ratione similique tempore, voluptatibus.
+                            </p>
+                            <button className="main-btn">Click me</button>
+                        </div>
+                    </div>
+
+                </section>
+
+            </main>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,25 @@
+function Navigation() {
+    return (
+        <div className="c-container">
+            <nav className="nav">
+                <div className="nav ">
+                    <a className="navbar-brand" aria-current="page" href="#">🌟</a>
+                </div>
+                <ul className="nav nav-list">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    );
+}
+
+export default Navigation;
