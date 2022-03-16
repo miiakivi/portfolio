865b55bc3a6e2c05c499f339c70da5f86a229e75
Miia Nyy <66413480+MiiaNyy@users.noreply.github.com>
2022-03-16 14:35:02 +0200

    updated readme

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 # Portfolio 
 
+Hi, I am Miia Nyyssönen and this is my portfolio. 
+
+I am newly graduated software developer who is passionate about good user expirience and simple but powerful design. 
+In this portfolio you can browse my most recent projects and learn a little more about me.
+
+Page is created using React, TypeScript and CSS.
+
 ### [Live version](https://miianyy.github.io/portfolio/)
+
+
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,19 @@
 import React, { Suspense } from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "animate.css/animate.min.css";
 
 import './index.css';
 
-import profile from "./assets/profile.jpg";
-import { BrowserRouter, Route, Routes, } from "react-router-dom";
-
 import App from './App';
 
-function ProfileImage() {
-    return <img src={profile} alt="profile img"/>
-}
 
-render(
-    <BrowserRouter>
-        <React.StrictMode>
-            <Suspense fallback="loading...">
-                <Routes>
-                    <Route path="/" element={ <App/> }/>
-                    <Route path="/profile.jpg" element={ <ProfileImage/> }/>
-                </Routes>
-            </Suspense>
-        </React.StrictMode>
-    </BrowserRouter>,
+ReactDOM.render(
+    <React.StrictMode>
+        <Suspense fallback="loading...">
+            <App/>
+        </Suspense>
+    </React.StrictMode>,
     document.getElementById( 'root' )
 );
 
