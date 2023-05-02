7b177c7f77eb38e70047eddf2c65a0630f483dba
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-02 17:14:40 +0300

    refactor: replaced type for type in the types folder. Also formatted file

--- src/components/SkillIcon.tsx
+++ src/components/SkillIcon.tsx
@@ -1,22 +1,16 @@
-import React from "react";
 import { AnimationOnScroll } from "react-animation-on-scroll";
 
-interface SkillIconProps {
-    skill : {
-        name : string;
-        icon : string;
-    }
-}
+import type { SkillIcon as SkillIconProps } from "../../types";
 
-function SkillIcon( {skill} : SkillIconProps ) {
-    return (
-        <AnimationOnScroll animateOnce={ true } animateIn="animate__fadeInUp">
-            <div className="icon-grid__item">
-                <i className={ `${ skill.icon } icon-grid__item-icon` }/>
-                <p>{ skill.name }</p>
-            </div>
-        </AnimationOnScroll>
-    )
+function SkillIcon( { skill }: SkillIconProps ) {
+  return (
+    <AnimationOnScroll animateOnce={true} animateIn="animate__fadeInUp">
+      <div className="icon-grid__item">
+        <i className={`${skill.icon} icon-grid__item-icon`} />
+        <p>{skill.name}</p>
+      </div>
+    </AnimationOnScroll>
+  );
 }
 
 export default SkillIcon;
