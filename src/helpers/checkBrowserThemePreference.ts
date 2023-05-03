dfff1b6ca8bf6c55309d8d4321a216696c8da665
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-03 12:00:37 +0300

    refactor: removed redundant console logs and else statement

--- src/helpers/checkBrowserThemePreference.ts
+++ src/helpers/checkBrowserThemePreference.ts
@@ -3,23 +3,21 @@ function checkBrowserThemePreference(
   setIsDarkMode: ( isDarkMode: boolean ) => void
 ) {
   // Check to see if Media-Queries are supported
-
   if ( window.matchMedia ) {
     // Check if the dark-mode Media-Query matches
     if ( window.matchMedia( "(prefers-color-scheme: dark)" ).matches ) {
       // Dark
       setIsDarkMode( true );
-      console.log( "prefers dark mode" );
+      return;
     } else {
       // Light
       setIsDarkMode( false );
-      console.log( "prefers light mode" );
+      return;
     }
-  } else {
-    // Default (when Media-Queries are not supported)
-    setIsDarkMode( true );
-    console.log( "not preferences" );
   }
+  // Default (when Media-Queries are not supported)
+  setIsDarkMode( true );
+  console.log( "not preferences" );
 }
 
 export default checkBrowserThemePreference;
