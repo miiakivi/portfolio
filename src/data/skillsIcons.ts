5cc27057ca72f476ed4dd592525a9202c5adfa99
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-03 12:50:25 +0300

    feat: formated file and added couple more skill icons

--- src/data/skillsIcons.ts
+++ src/data/skillsIcons.ts
@@ -1,84 +1,100 @@
 const skillsIcons = [
-    {
-        name: "javascript",
-        icon: "devicon-javascript-plain"
-    },
-    {
-        name: "typescript",
-        icon: "devicon-typescript-original"
-    },
-    {
-        name: "react",
-        icon: "devicon-react-original"
-    },
-    {
-        name: "nodejs",
-        icon: "devicon-nodejs-plain"
-    },
-    {
-        name: "express",
-        icon: "devicon-express-original"
-    },
-    {
-        name: "mongodb",
-        icon: "devicon-mongodb-plain"
-    },
-    {
-        name: "firebase",
-        icon: "devicon-firebase-plain"
-    },
-    {
-        name: "webpack",
-        icon: "devicon-webpack-plain"
-    },
-    {
-        name: "azure",
-        icon: "devicon-azure-plain"
-    },
-    {
-        name: "heroku",
-        icon: "devicon-heroku-original"
-    },
-    {
-        name: "jest",
-        icon: "devicon-jest-plain"
-    },
-    {
-        name: "github",
-        icon: "devicon-github-original"
-    },
-    {
-        name: "git",
-        icon: "devicon-git-plain"
-    },
-    {
-        name: "handlebars",
-        icon: "devicon-handlebars-plain"
-    },
-    {
-        name: "bootstrap",
-        icon: "devicon-bootstrap-plain"
-    },
-    {
-        name: "html5",
-        icon: "devicon-html5-plain"
-    },
-    {
-        name: "css3",
-        icon: "devicon-css3-plain-wordmark"
-    },
-    {
-        name: "tailwind",
-        icon: "devicon-tailwindcss-plain"
-    },
-    {
-        name: "sass",
-        icon: "devicon-sass-original"
-    },
-    {
-        name: "canva",
-        icon: "devicon-canva-original",
-    },
+  {
+    name: "javascript",
+    icon: "devicon-javascript-plain"
+  },
+  {
+    name: "typescript",
+    icon: "devicon-typescript-original"
+  },
+  {
+    name: "vue",
+    icon: "devicon-vuejs-plain"
+  },
+  {
+    name: "react",
+    icon: "devicon-react-original"
+  },
+  {
+    name: "backbone",
+    icon: "devicon-backbonejs-plain"
+  },
+  {
+    name: "jquery",
+    icon: "devicon-jquery-plain-wordmark"
+  },
+  {
+    name: "nodejs",
+    icon: "devicon-nodejs-plain"
+  },
+  {
+    name: "express",
+    icon: "devicon-express-original"
+  },
+  {
+    name: "mongodb",
+    icon: "devicon-mongodb-plain"
+  },
+  {
+    name: "firebase",
+    icon: "devicon-firebase-plain"
+  },
+  {
+    name: "webpack",
+    icon: "devicon-webpack-plain"
+  },
+  {
+    name: "azure",
+    icon: "devicon-azure-plain"
+  },
+  {
+    name: "heroku",
+    icon: "devicon-heroku-original"
+  },
+  {
+    name: "jest",
+    icon: "devicon-jest-plain"
+  },
+  {
+    name: "github",
+    icon: "devicon-github-original"
+  },
+  {
+    name: "git",
+    icon: "devicon-git-plain"
+  },
+  {
+    name: "handlebars",
+    icon: "devicon-handlebars-plain"
+  },
+  {
+    name: "bootstrap",
+    icon: "devicon-bootstrap-plain"
+  },
+  {
+    name: "html5",
+    icon: "devicon-html5-plain"
+  },
+  {
+    name: "css3",
+    icon: "devicon-css3-plain-wordmark"
+  },
+  {
+    name: "less",
+    icon: "devicon-less-plain-wordmark"
+  },
+  {
+    name: "sass",
+    icon: "devicon-sass-original"
+  },
+  {
+    name: "figma",
+    icon: "devicon-figma-plain"
+  },
+  {
+    name: "canva",
+    icon: "devicon-canva-original"
+  }
 ];
 
 export default skillsIcons;
