583b7436d99c851caee6de8229eecfe6679eb08a
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-05 17:15:27 +0300

    style: formated file

--- src/locales/locale-fin.ts
+++ src/locales/locale-fin.ts
@@ -1,59 +1,64 @@
 export default {
-    "navigation": {
-        "about": "Minusta",
-        "projects": "projektit",
-        "skills": "Taidot",
-        "contact": "Ota yhteyttä"
+  navigation: {
+    about: "Minusta",
+    experience: "työkokemus",
+    projects: "projektit",
+    skills: "Taidot",
+    contact: "Ota yhteyttä"
+  },
+  hero: {
+    title: "Hei, olen",
+    subtitle: "Ohjelmistokehittäjä",
+    and: "ja",
+    "words-animation": {
+      word1: "reissaaja",
+      word2: "kokki",
+      word3: "optimisti",
+      word4: "ukulelen soittaja"
     },
-    "hero": {
-        "title": "Hei, olen",
-        "subtitle": "Ohjelmistokehittäjä",
-        "and": "ja",
-        "words-animation": {
-            "word1": "reissaaja",
-            "word2": "kokki",
-            "word3": "optimisti",
-            "word4": "ukulelen soittaja"
-        },
-        "button": "ota yhteyttä"
+    button: "ota yhteyttä"
+  },
+  about: {
+    title: "Minusta",
+    subtitle: "Kukas minä olinkaan",
+    paragraph1:
+      "Olen vastavalmistunut ohjelmistokehittäjä jolle hyvä käyttäjäkokemus ja helppolukuinen koodi on tärkeää jokaisessa projektissa. ",
+    paragraph2:
+      "Kun en istu läppärin edessä, suunnittelen seuraavaa reissukohdetta, käyn entisen katukoiran" +
+      " (nykyisen kotikoiran) kanssa lenkillä tai sometan instagramissa (aivan liikaa).",
+    paragraph3:
+      "Olen tällä hetkellä etsimässä itselleni ensimmäistä alan työpaikkaa. Tulevalta työpaikaltani toivoisin mukavaa ja kannustavaa työilmapiiriä ja etätyömahdollisuutta.",
+    paragraph4:
+      "Jos kiinnostuit tai muuten vaan haluat sanoa moikka, niin ota rohkeasti yhteyttä.",
+    button: "Ota yhteyttä"
+  },
   experience: {
     title: "Työkokemus",
     subtitle: "Missä kaikkialla olen ollut töissä"
   },
+  projects: {
+    title: "Projektit",
+    subtitle: "kokoelma projekteja, joita olen rakentanut"
+  },
+  projectDetails: {
+    button: "Lue lisää"
+  },
+  skills: {
+    title: "Taidot",
+    subtitle: "Tutut teknologiat"
+  },
+  footer: {
+    lets: "haluatko",
+    "words-animation": {
+      word1: "palkata minut",
+      word2: "jutella lisää",
+      word3: "tavata Teamsissä",
+      word4: "käydä kahvilla"
     },
-    "about": {
-        "title": "Minusta",
-        "subtitle": "Kukas minä olinkaan",
-        "paragraph1": "Olen vastavalmistunut ohjelmistokehittäjä jolle hyvä käyttäjäkokemus ja helppolukuinen koodi on tärkeää jokaisessa projektissa. ",
-        "paragraph2": "Kun en istu läppärin edessä, suunnittelen seuraavaa reissukohdetta, käyn entisen katukoiran" +
-            " (nykyisen kotikoiran) kanssa lenkillä tai sometan instagramissa (aivan liikaa).",
-        "paragraph3": "Olen tällä hetkellä etsimässä itselleni ensimmäistä alan työpaikkaa. Tulevalta työpaikaltani toivoisin mukavaa ja kannustavaa työilmapiiriä ja etätyömahdollisuutta.",
-        "paragraph4": "Jos kiinnostuit tai muuten vaan haluat sanoa moikka, niin ota rohkeasti yhteyttä.",
-        "button": "Ota yhteyttä"
+    cta: {
+      part1: "Lähetä mulle ",
+      part2: " maili"
     },
-    "projects": {
-        "title": "Projektit",
-        "subtitle": "kokoelma projekteja, joita olen rakentanut"
-    },
-    "projectDetails": {
-        "button": "Lue lisää"
-    },
-    "skills": {
-        "title": "Taidot",
-        "subtitle": "Tutut teknologiat"
-    },
-    "footer": {
-        "lets": "haluatko",
-        "words-animation": {
-            "word1": "palkata minut",
-            "word2": "jutella lisää",
-            "word3": "tavata Teamsissä",
-            "word4": "käydä kahvilla"
-        },
-        "cta": {
-            "part1": "Lähetä mulle ",
-            "part2": " maili"
-        },
-        "socials-title": "tai vaan sano moikka!"
-    }
-}
+    "socials-title": "tai vaan sano moikka!"
+  }
+};
