0b21e510219f218c4c3104bc194446923cf5907f
Miia Nyyssönen <mi.nyyssonen@gmail.com>
2023-05-05 17:12:50 +0300

    feat: added work experience data

---
+++ src/data/workExperience.ts
@@ -0,0 +1,52 @@
+const workExperience = [
+  {
+    date: "11/2022 - 04/2023",
+    company: "Vello Solutions Oy",
+    text: {
+      en: {
+        title: "Frontend Developer",
+        description:
+          "Contributed to the development of an online appointment app. Created new features that enabled medical professionals to add patient entries to Kanta directly through the app, using my expertise in Javascript, Typescript, BackboneJS, and VueJS. I worked both independently and as part of a small team and made valuable contributions to the success of the project."
+      },
+      fin: {
+        title: "Frontend Kehittäjä",
+        description:
+          "Olin mukana kehittämässä online-ajanvaraussovellusta. Olin mukana luomassa uusia ominaisuuksia, joiden avulla mm. lääketieteen ammattilaiset pystyivät lisäämään potilastietoja Kantaan suoraan sovelluksen kautta. Työssäni käytin Javascriptin, Typescriptin, BackboneJS:n ja VueJS:n asiantuntemustani. Työskentelin sekä itsenäisesti että osana pientä tiimiä."
+      }
+    }
+  },
+  {
+    date: "08/2021 - 02/2022",
+    company: "Kuntien Tiera",
+    text: {
+      en: {
+        title: "Intern",
+        description:
+          "As part of the team, I developed an internal virtual assistant for Tiera HR using Azure Bot Services. My responsibilities included managing and creating the QnA Maker Knowledge Base and integrating natural language processing (LUIS) into the assistant."
+      },
+      fin: {
+        title: "Harjoittelija",
+        description:
+          "Olin osana tiimiä, joka rakensi sisäisen virtuaaliavustajan Tiera HR:lle käyttäen Azure Bot Services -palveluja. Vastuullani oli QnA Maker Knowledge Basen hallinta ja luominen sekä luonnollisen kielen prosessoinnin (LUIS) integrointi avustajaan."
+      }
+    }
+  },
+  {
+    date: "2016 - 2020",
+    company: "Multiple restaurants",
+    text: {
+      en: {
+        title: "Chef and Customer server",
+        description:
+          "As a chef and customer server, I gained valuable experience in teamwork and customer service. Working closely with the wait- and kitchen staff to ensure efficient service required strong teamwork skills, as well as the ability to work independently and manage my own schedule. Interacting with customers to take orders and answer questions helped me to develop excellent customer service skills."
+      },
+      fin: {
+        title: "Kokki ja asiakaspalvelija",
+        description:
+          "Kokkina ja asiakaspalvelijana sain arvokasta kokemusta tiimityöstä ja asiakaspalvelusta. Opin työskentelemään stressaavissa ja kiireisissä tilanteissa, aikatauluttamaan oman työskentelyni sekä ottamaan vastuun omasta työstäni ja sen panoksesta. Asiakkaiden kanssa vuorovaikutuksessa tilausten ottaminen ja kysymyksiin vastaaminen auttoi minua kehittämään erinomaiset asiakaspalvelutaidot."
+      }
+    }
+  }
+];
+
+export default workExperience;
